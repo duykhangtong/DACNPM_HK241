f82b5541fea62c7f3fa7548af61cecec7ceb8c58
ZhangNguyen <khang.nguyenminh2k4@gmail.com>
2024-11-08 21:24:02 +0700

    up code

---
+++ frontend/my-react-app/src/App.jsx
@@ -0,0 +1,10 @@
+import Login from './Login/Login.jsx'
+import SPSS from './SPSS/SPSS.jsx'
+import SPSO from './SPSO/SPSO.jsx'
+function App() {
+return (
+        <SPSS/>
+);
+}
+
+export default App
