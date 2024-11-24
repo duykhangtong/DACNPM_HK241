0cd16570f37fdb31211e6b0748a70ce16ce42cf4
Vũ Xuân Chính <chinh.vuxuan852004@hcmut.edu.vn>
2024-11-24 13:55:13 +0700

    filter by Printer

--- back-end/src/routers/printer.route.js
+++ back-end/src/routers/printer.route.js
@@ -4,6 +4,7 @@ const printerController = require('../controllers/printer.controller');
 
 router.delete('/:id', printerController.removeById);
 router.put('/:id', printerController.updateState);
+router.get('/filter', printerController.filterPrinter);
 router.post('/', printerController.create);
 router.get('/', printerController.getAll);
 
