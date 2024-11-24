4b37ce9eaafe3e93856319e1615a8c425dead60f
Vũ Xuân Chính <chinh.vuxuan852004@hcmut.edu.vn>
2024-11-24 13:51:11 +0700

    add number page

--- back-end/src/controllers/page_order.controller.js
+++ back-end/src/controllers/page_order.controller.js
@@ -1,4 +1,6 @@
 const PageOrder = require('../models/page_order.model');
+const Client = require('../models/client.model');
+const { client } = require('../models');
 
 const create = (req, res, next) => {
     const { number_of_page, state, client_id } = req.body;
@@ -15,4 +17,16 @@ const getAll = (req, res, next) => {
         .catch(err => next(err));
 }
 
-module.exports = { create, getAll };
+// [PATCH] /api/pageOrders/update/:id.:number_page
+const update = async(req, res, next) => {
+    try {
+        const { id, number_page } = req.params;
+        
+        const updateUser = await Client.findByIdAndUpdate({ _id: id }, { $inc: { number_page } });
+        const user = await Client.findById({ _id: id });
+        res.status(200).json(user); 
+    } catch (err) {
+        next(err);
+    }
+}
+module.exports = { create, getAll, update };
