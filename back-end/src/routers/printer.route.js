e910d8683ca7cdf32a2b83d83982f036bf7523ff
duykhangtong <khang.tong1124@hcmut.edu.vn>
2024-12-27 09:04:59 +0700

    chore: remove authen printer

--- back-end/src/routers/printer.route.js
+++ back-end/src/routers/printer.route.js
@@ -3,10 +3,14 @@ const router = express.Router();
 const printerController = require('../controllers/printer.controller');
 const authJwt = require('../middleware/authJwt');
 
-router.delete('/:id', [authJwt.AdminVerifyToken], printerController.removeById);
-router.put('/:id', [authJwt.AdminVerifyToken], printerController.updateState);
+
+router.delete('/:id', printerController.removeById);
+router.put('/:id', printerController.updateState);
+//router.delete('/:id', [authJwt.AdminVerifyToken], printerController.removeById);
+//router.put('/:id', [authJwt.AdminVerifyToken], printerController.updateState);
 router.get('/filter', printerController.filterPrinter);
-router.post('/', [authJwt.AdminVerifyToken], printerController.create);
+router.post('/', printerController.create);
+//router.post('/', [authJwt.AdminVerifyToken], printerController.create);
 router.get('/', printerController.getAll);
 
 module.exports = router;
