d0dfb0217b622b76bbd0c50779e498b8a6e80c24
duykhangtong <khang.tong1124@hcmut.edu.vn>
2024-12-31 14:41:18 +0700

    feat: update page number client

--- back-end/src/controllers/account.controller.js
+++ back-end/src/controllers/account.controller.js
@@ -8,10 +8,24 @@ const getClientInfo = (req, res) => {
         .catch(error => console.error(error));
 }
 
+const updateNumberPage = async (req, res) => {
+    try {
+        const { total_print_pages } = req.body;
+        const client_id = req.role;
+        await Client.findByIdAndUpdate(
+            client_id,
+            { $inc: { number_page: -total_print_pages } }
+        );
+        res.json({ 'message': 'update successfully!!' });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
 const getAdminInfo = (req, res) => {
     Spso.findById(req.role)
         .then(spso => res.status(200).json(spso))
         .catch(error => console.error(error));
 }
 
-module.exports = { getClientInfo, getAdminInfo };
+module.exports = { getClientInfo, getAdminInfo, updateNumberPage };
