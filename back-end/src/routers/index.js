884c246e20de0e7d7e669153e08ff3e5b2035f76
duykhangtong <khang.tong1124@hcmut.edu.vn>
2025-01-02 21:52:23 +0700

    update(document, feedback): remove all

--- back-end/src/routers/index.js
+++ back-end/src/routers/index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 const authenRoute = require('./authen.route');
-const documentRoute = require('./document.route');
-const feedbackRoute = require('./feedback.route');
 const printerRoute = require('./printer.route');
 const pageOrderRoute = require('./page_order.route');
 const printOrderRoute = require('./print_order.route');
@@ -12,8 +10,6 @@ const fileRoute = require('./file.route');
 
 initWebRouter = (app) => {
     router.use('/auth', authenRoute);
-    router.use('/documents', documentRoute);
-    router.use('/feedbacks', feedbackRoute);
     router.use('/printers', printerRoute);
     router.use('/pageOrders', pageOrderRoute);
     router.use('/printOrders', printOrderRoute);
