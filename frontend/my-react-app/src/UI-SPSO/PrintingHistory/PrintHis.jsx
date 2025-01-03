7cfa6294cf9ed9c854ab2c5c72b7e3daeb0080fb
CanNguyen284 <nguyen.daocan@hcmut.edu.vn>
2025-01-03 15:52:14 +0700

    update UI-SPSO

--- frontend/my-react-app/src/UI-SPSO/PrintingHistory/PrintHis.jsx
+++ frontend/my-react-app/src/UI-SPSO/PrintingHistory/PrintHis.jsx
@@ -16,6 +16,10 @@ function PrintHistoryFilter() {
   const [filteredPrintOrders, setFilteredPrintOrders] = useState([]);
   const [isFiltered, setIsFiltered] = useState(false);
 
+  // Phân trang
+  const [currentPage, setCurrentPage] = useState(1);
+  const ordersPerPage = 5; // Số lượng đơn in trên mỗi trang
+
   useEffect(() => {
     const fetchClients = async () => {
       try {
@@ -61,10 +65,12 @@ function PrintHistoryFilter() {
 
     const fetchPrintOrders = async () => {
       try {
+        const token = localStorage.getItem("token");
         const response = await axios.get(
           "http://localhost:80/api/printOrders",
           {
             headers: {
+              Authorization: `Bearer ${token}`,
               "Content-Type": "application/json",
             },
           }
@@ -92,25 +98,35 @@ function PrintHistoryFilter() {
           params: {
             client_id: selectedClient,
             printer_id: selectedPrinter,
-            start_date: startDate ? startDate.toISOString().split("T")[0] : null,
+            start_date: startDate
+              ? startDate.toISOString().split("T")[0]
+              : null,
             end_date: endDate ? endDate.toISOString().split("T")[0] : null,
           },
         }
       );
 
-      const filteredData = response.data.filter(order => {
-        const orderStartDate = new Date(order.createdAt).toISOString().split('T')[0];
-        const orderEndDate = order.end_date ? new Date(order.end_date).toISOString().split('T')[0] : null;
+      const filteredData = response.data.filter((order) => {
+        const orderStartDate = new Date(order.createdAt)
+          .toISOString()
+          .split("T")[0];
+        const orderEndDate = order.end_date
+          ? new Date(order.end_date).toISOString().split("T")[0]
+          : null;
 
-        const isStartDateMatch = startDate ? orderStartDate > startDate.toISOString().split('T')[0] : true;
-        const isEndDateMatch = endDate ? orderEndDate <= endDate.toISOString().split('T')[0] : true;
+        const isStartDateMatch = startDate
+          ? orderStartDate > startDate.toISOString().split("T")[0]
+          : true;
+        const isEndDateMatch = endDate
+          ? orderEndDate <= endDate.toISOString().split("T")[0]
+          : true;
 
         return isStartDateMatch && isEndDateMatch;
       });
 
       setFilteredPrintOrders(filteredData);
       setIsFiltered(true);
-      console.log(filteredData);
+      setCurrentPage(1); // Reset trang khi lọc
     } catch (error) {
       console.error("Error fetching filtered print orders:", error);
     }
@@ -134,108 +150,184 @@ function PrintHistoryFilter() {
   const getPrinterLocationById = (id) => {
     const printer = printers.find((p) => p._id === id);
     return printer
-      ? { campus: printer.campus, building: printer.building, room: printer.room }
+      ? {
+          campus: printer.campus,
+          building: printer.building,
+          room: printer.room,
+        }
       : { campus: "Unknown", building: "Unknown", room: "Unknown" };
+  };
+
+  const updatePrintOrderStatus = async (orderId) => {
+    try {
+      const token = localStorage.getItem("token");
+      console.log(token);
+      await axios.put(
+        `http://localhost:80/api/printOrders/:{orderId}`,
+        {
+          state: "complete",
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      setPrintOrders((prevOrders) =>
+        prevOrders.map((order) =>
+          order._id === orderId ? { ...order, state: "complete" } : order
+        )
+      );
+      alert("Trạng thái đã được cập nhật thành công!");
+    } catch (error) {
+      console.error("Error updating print order status:", error);
+      alert("Có lỗi xảy ra khi cập nhật trạng thái.");
+    }
   };
 
   const ordersToDisplay = isFiltered ? filteredPrintOrders : printOrders;
 
+  // Tính toán các đơn in cần hiển thị cho trang hiện tại
+  const indexOfLastOrder = currentPage * ordersPerPage;
+  const indexOfFirstOrder = indexOfLastOrder - ordersPerPage;
+  const currentOrders = ordersToDisplay.slice(
+    indexOfFirstOrder,
+    indexOfLastOrder
+  );
+  const totalPages = Math.ceil(ordersToDisplay.length / ordersPerPage);
+
   return (
-    <div className="his-container">
-      <h2>Lịch sử in</h2>
-      <div className="filter-container">
-        <div className="sub-filter">
-          <label>Chọn tên</label>
-          <select
-            value={selectedClient}
-            onChange={(e) => setSelectedClient(e.target.value)}
-          >
-            <option value="">Chọn tên sinh viên</option>
-            {Array.isArray(clients) &&
-              clients.map((client) => (
-                <option key={client._id} value={client._id}>
-                  {client.full_name}
-                </option>
-              ))}
-          </select>
-        </div>
-        <div className="sub-filter">
-          <label>Chọn máy in</label>
-          <select
-            value={selectedPrinter}
-            onChange={(e) => setSelectedPrinter(e.target.value)}
-          >
-            <option value="">Chọn máy in</option>
-            {Array.isArray(printers) &&
-              printers.map((printer) => (
-                <option key={printer._id} value={printer._id}>
-                  {printer.name}
-                </option>
-              ))}
-          </select>
-        </div>
-        <div className="sub-filter">
-          <label>Chọn ngày bắt đầu</label>
-          <DatePicker
-            selected={startDate}
-            onChange={(date) => setStartDate(date)}
-            dateFormat="dd/MM/yyyy"
-            placeholderText="dd/mm/yyyy"
-          />
-        </div>
-        <div className="sub-filter">
-          <label>Chọn ngày kết thúc</label>
-          <DatePicker
-            selected={endDate}
-            onChange={(date) => setEndDate(date)}
-            dateFormat="dd/MM/yyyy"
-            placeholderText="dd/mm/yyyy"
-          />
-        </div>
-      </div>
-      <button onClick={filterPrintOrders}>Xác nhận</button>
-      {ordersToDisplay.length === 0 ? (
-        <p>Không có đơn in nào</p>
-      ) : (
-        <table className="print-history-table">
-          <thead>
-            <tr>
-              <th>Tên tài liệu</th>
-              <th>Tên người dùng</th>
-              <th>Tên máy in</th>
-              <th>Cơ sở</th>
-              <th>Tòa nhà</th>
-              <th>Phòng</th>
-              <th>Kích cỡ trang</th>
-              <th>Ngày bắt đầu đặt in</th>
-              <th>Ngày in xong</th>
-              <th>Tổng số trang in</th>
-              <th>Trạng thái</th>
-            </tr>
-          </thead>
-          <tbody>
-            {ordersToDisplay.map((order) => {
-              const location = getPrinterLocationById(order.printer_id);
-              return (
-                <tr key={order._id}>
-                  <td>{getDocumentNameById(order.file_id)}</td>
-                  <td>{getClientNameById(order.client_id)}</td>
-                  <td>{getPrinterNameById(order.printer_id)}</td>
-                  <td>{location.campus}</td>
-                  <td>{location.building}</td>
-                  <td>{location.room}</td>
-                  <td>{order.page_size}</td>
-                  <td>{order.createdAt ? new Date(order.createdAt).toLocaleString() : "Unknown"}</td>
-                  <td>{order.end_date ? new Date(order.end_date).toLocaleString() : "Unknown"}</td>
-                  <td>{order.total_print_pages}</td>
-                  <td>{order.state}</td>
+    <>
+      <main>
+        <div className="his-container">
+          <h2>Lịch sử in</h2>
+          <div className="filter-container">
+            <div className="sub-filter">
+              <label>Chọn tên</label>
+              <select
+                value={selectedClient}
+                onChange={(e) => setSelectedClient(e.target.value)}
+              >
+                <option value="">Chọn tên sinh viên</option>
+                {Array.isArray(clients) &&
+                  clients.map((client) => (
+                    <option key={client._id} value={client._id}>
+                      {client.full_name}
+                    </option>
+                  ))}
+              </select>
+            </div>
+            <div className="sub-filter">
+              <label>Chọn máy in</label>
+              <select
+                value={selectedPrinter}
+                onChange={(e) => setSelectedPrinter(e.target.value)}
+              >
+                <option value="">Chọn máy in</option>
+                {Array.isArray(printers) &&
+                  printers.map((printer) => (
+                    <option key={printer._id} value={printer._id}>
+                      {printer.name}
+                    </option>
+                  ))}
+              </select>
+            </div>
+            <div className="sub-filter">
+              <label>Chọn ngày bắt đầu</label>
+              <DatePicker
+                selected={startDate}
+                onChange={(date) => setStartDate(date)}
+                dateFormat="dd/MM/yyyy"
+                placeholderText="dd/mm/yyyy"
+              />
+            </div>
+            <div className="sub-filter">
+              <label>Chọn ngày kết thúc</label>
+              <DatePicker
+                selected={endDate}
+                onChange={(date) => setEndDate(date)}
+                dateFormat="dd/MM/yyyy"
+                placeholderText="dd/mm/yyyy"
+              />
+            </div>
+          </div>
+          <button onClick={filterPrintOrders}>Xác nhận</button>
+          {currentOrders.length === 0 ? (
+            <p>Không có đơn in nào</p>
+          ) : (
+            <table className="print-history-table">
+              <thead>
+                <tr>
+                  <th>Tên tài liệu</th>
+                  <th>Tên người dùng</th>
+                  <th>Tên máy in</th>
+                  <th>Cơ sở</th>
+                  <th>Tòa nhà</th>
+                  <th>Phòng</th>
+                  <th>Kích cỡ trang</th>
+                  <th>Ngày bắt đầu đặt in</th>
+                  <th>Ngày in xong</th>
+                  <th>Tổng số trang in</th>
+                  <th>Trạng thái</th>
+                  <th>Xác nhận hoàn tất</th>
                 </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      )}
-    </div>
+              </thead>
+              <tbody>
+                {currentOrders.map((order) => {
+                  const location = getPrinterLocationById(order.printer_id);
+                  return (
+                    <tr key={order._id}>
+                      <td>{getDocumentNameById(order.file_id)}</td>
+                      <td>{getClientNameById(order.client_id)}</td>
+                      <td>{getPrinterNameById(order.printer_id)}</td>
+                      <td>{location.campus}</td>
+                      <td>{location.building}</td>
+                      <td>{location.room}</td>
+                      <td>{order.page_size}</td>
+                      <td>
+                        {order.createdAt
+                          ? new Date(order.createdAt).toLocaleString()
+                          : "Unknown"}
+                      </td>
+                      <td>
+                        {order.end_date
+                          ? new Date(order.end_date).toLocaleString()
+                          : "Unknown"}
+                      </td>
+                      <td>{order.total_print_pages}</td>
+                      <td>{order.state}</td>
+                      <td>
+                        {order.state === "pending" && (
+                          <button
+                            onClick={() => updatePrintOrderStatus(order._id)}
+                          >
+                            Hoàn tất
+                          </button>
+                        )}
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          )}
+          <div className="pagination">
+            {Array.from({ length: totalPages }, (_, index) => (
+              <button
+                key={index + 1}
+                className={`page-item ${
+                  currentPage === index + 1 ? "active" : ""
+                }`}
+                onClick={() => setCurrentPage(index + 1)}
+              >
+                {index + 1}
+              </button>
+            ))}
+          </div>
+        </div>
+      </main>
+    </>
   );
 }
 
--- frontend/my-react-app/src/UI-SPSO/Report-SPSO/Report.jsx
+++ frontend/my-react-app/src/UI-SPSO/Report-SPSO/Report.jsx
@@ -4,48 +4,49 @@ import "./Report.css";
 import { useState, useEffect } from "react";
 import logoBK from "../../../Image/logo_BK2-removebg.png";
 import axios from "axios";
+
 function Report() {
   const months = Array.from({ length: 12 }, (_, i) => i + 1);
-  const years = Array.from({ length: 10 }, (_, i) => 2024 - i); // Hiển thị từ 2024 đến 2015
+  const years = Array.from({ length: 10 }, (_, i) => 2025 - i);
 
-  const [selectedReportType, setSelectedReportType] = useState("");
   const [selectedMonth, setSelectedMonth] = useState("");
   const [selectedYear, setSelectedYear] = useState("");
   const [statisticsData, setStatisticsData] = useState([]);
-  useEffect(() => {
-    const fetchStatisticsData = async () => {
-      try {
-        const response = await axios.get("http://localhost:80/api/statistics", {
-          headers: { "Content-Type": "application/json" },
-        });
-        setStatisticsData(response.data);
-      } catch (error) {
-        console.error("Error fetching statistics data:", error);
-      }
-    };
-    fetchStatisticsData();
-  }, []);
+  const [showStatistics, setShowStatistics] = useState(false); // New state
+
+  const fetchReportData = async (month, year) => {
+    try {
+      const response = await axios.get("http://localhost:80/api/report", {
+        params: { month, year },
+      });
+      const statisticsArray = Object.entries(response.data)
+        .filter(([key]) => key !== 'total')
+        .map(([, value]) => value);
+      setStatisticsData(statisticsArray);
+    } catch (error) {
+      console.error("Error fetching report data:", error);
+      setStatisticsData([]);
+    }
+  };
+
+  const handleDetailClick = (month, year) => {
+    setSelectedMonth(month);
+    setSelectedYear(year);
+    fetchReportData(month, year);
+    setShowStatistics(true); // Show statistics table
+  };
+
+  const handleBackClick = () => {
+    setShowStatistics(false); // Hide statistics table
+  };
+
   return (
     <div className="d-flex flex-column min-vh-100">
       <main className="flex-grow-1">
         <div className="container py-4">
           <div className="card">
             <div className="header">
-              <div className="col">
-                <label className="text-gray-700">Chọn loại báo cáo</label>
-                <select
-                  className="form-select"
-                  value={selectedReportType}
-                  onChange={(e) => setSelectedReportType(e.target.value)}
-                >
-                  <option value="" disabled>
-                    Chọn loại báo cáo
-                  </option>
-                  <option value="thang">Theo tháng</option>
-                  <option value="nam">Theo năm</option>
-                  <option value="tatca">Tất cả</option>
-                </select>
-              </div>
+              
               <div className="col">
                 <label className="text-gray-700">Chọn theo tháng</label>
                 <select
@@ -92,115 +93,78 @@ function Report() {
               </thead>
               <tbody>
                 {[
-                  {
-                    id: 1,
-                    month: 10,
-                    year: 2024,
-                    detail: "Báo cáo tháng 10/2024",
-                  },
-                  {
-                    id: 2,
-                    month: 9,
-                    year: 2024,
-                    detail: "Báo cáo tháng 9/2024",
-                  },
-                  {
-                    id: 3,
-                    month: 8,
-                    year: 2024,
-                    detail: "Báo cáo tháng 8/2024",
-                  },
-                  {
-                    id: 4,
-                    month: 7,
-                    year: 2024,
-                    detail: "Báo cáo tháng 7/2024",
-                  },
-                  {
-                    id: 5,
-                    month: 6,
-                    year: 2024,
-                    detail: "Báo cáo tháng 6/2024",
-                  },
-                  {
-                    id: 6,
-                    month: 5,
-                    year: 2024,
-                    detail: "Báo cáo tháng 5/2024",
-                  },
-                  {
-                    id: 7,
-                    month: 4,
-                    year: 2024,
-                    detail: "Báo cáo tháng 4/2024",
-                  },
-                  {
-                    id: 8,
-                    month: 3,
-                    year: 2024,
-                    detail: "Báo cáo tháng 3/2024",
-                  },
-                  {
-                    id: 9,
-                    month: 2,
-                    year: 2024,
-                    detail: "Báo cáo tháng 2/2024",
-                  },
+                  { id: 1, month: 1, year: 2025, detail: "Báo cáo tháng 1/2025" },
+                  { id: 2, month: 9, year: 2024, detail: "Báo cáo tháng 9/2024" },
+                  { id: 3, month: 8, year: 2024, detail: "Báo cáo tháng 8/2024" },
+                  { id: 4, month: 7, year: 2024, detail: "Báo cáo tháng 7/2024" },
+                  { id: 5, month: 6, year: 2024, detail: "Báo cáo tháng 6/2024" },
+                  { id: 6, month: 5, year: 2024, detail: "Báo cáo tháng 5/2024" },
+                  { id: 7, month: 4, year: 2024, detail: "Báo cáo tháng 4/2024" },
+                  { id: 8, month: 3, year: 2024, detail: "Báo cáo tháng 3/2024" },
+                  { id: 9, month: 2, year: 2024, detail: "Báo cáo tháng 2/2024" },
                 ].map((item) => (
                   <tr key={item.id}>
                     <td>{item.id}</td>
                     <td>{item.month}</td>
                     <td>{item.year}</td>
                     <td className="text-primary">
-                      <a href="#">{item.detail}</a>
+                      <a
+                        className="report"
+                        onClick={() => handleDetailClick(item.month, item.year)}
+                      >
+                        {item.detail}
+                      </a>
                     </td>
                   </tr>
                 ))}
               </tbody>
             </table>
           </div>
-        </div>
-        <div className="statistics-container">
-          {" "}
-          <h2>Báo cáo hệ thống</h2>{" "}
-          <table className="statistics-table">
-            {" "}
-            <thead>
-              {" "}
-              <tr>
-                {" "}
-                <th>STT</th> <th>Máy in</th> <th>Số đơn đặt hàng</th>{" "}
-                <th>Số trang giấy A3</th> <th>Số trang giấy A4</th>{" "}
-              </tr>{" "}
-            </thead>{" "}
-            <tbody>
-              {" "}
-              {statisticsData.length === 0 ? (
-                <tr>
-                  {" "}
-                  <td colSpan="5">Không có dữ liệu</td>{" "}
-                </tr>
-              ) : (
-                statisticsData.map((data, index) => (
-                  <tr key={data.id}>
-                    {" "}
-                    <td>{index + 1}</td> <td>{data.printerName}</td>{" "}
-                    <td>{data.orderCount}</td> <td>{data.a3Pages}</td>{" "}
-                    <td>{data.a4Pages}</td>{" "}
+          {showStatistics && ( // Only show the statistics table if showStatistics is true
+            <div className="statistics-container">
+              <h2>Báo cáo hệ thống</h2>
+             
+              <table className="statistics-table">
+                <thead>
+                  <tr>
+                    <th>STT</th>
+                    <th>Máy in</th>
+                    <th>Số đơn đặt hàng</th>
+                    <th>Số trang giấy A3</th>
+                    <th>Số trang giấy A4</th>
                   </tr>
-                ))
-              )}{" "}
-            </tbody>{" "}
-          </table>{" "}
+                </thead>
+                <tbody>
+                  {statisticsData.length === 0 ? (
+                    <tr>
+                      <td colSpan="5">Không có dữ liệu</td>
+                    </tr>
+                  ) : (
+                    statisticsData.map((data, index) => (
+                      <tr key={index}>
+                        <td>{index + 1}</td>
+                        <td>{data.printer}</td>
+                        <td>{data.amount}</td>
+                        <td>{data.number_A3}</td>
+                        <td>{data.number_A4}</td>
+                      </tr>
+                    ))
+                  )}
+                </tbody>
+              </table>
+               <button className="cancel" onClick={handleBackClick}>
+                Trở lại
+              </button>
+            </div>
+          )}
         </div>
       </main>
-
-      <footer className="footer">
-        <div className="footer-left d-flex align-items-center">
+           <footer className="footer">
+        <div className="left d-flex align-items-center">
           <img src={logoBK} alt="HCMUT logo" />
           <span className="info">HCMUT</span>
         </div>
-        <div className="footer-center">
+        <div className="center">
           <div className="info">
             <span>DANH MỤC</span>
           </div>
@@ -214,7 +178,7 @@ function Report() {
             <a href="lichsuin">Lịch sử dịch vụ</a>
           </div>
         </div>
-        <div className="footer-right">
+        <div className="right">
           <div className="info">
             <span>LIÊN HỆ</span>
           </div>
@@ -226,6 +190,7 @@ function Report() {
           </div>
         </div>
       </footer>
+
     </div>
   );
 }
